0b77d85b245c990c8f71fd5cc368fb489e6a1854
Bart Kowal <bartkw12@gmail.com>
2025-05-11 14:14:23 -0400

    adding configuration file for Q-learning with preset hyperparameters

---
+++ src/Q_learning_model/Q_learning_config.py
@@ -0,0 +1,8 @@
+# Hyperparameters setting
+
+# Q-Learning
+alpha = 0.5                  # learning rate - try 0.01 for more gradual learning
+gamma = 0.99                 # discount factor - for more short term focused tasks try 0.8
+epsilon = 1                  # exploration rate - full exploration rate at beginning = 1
+epsilon_decay = 0.998        # exploration decay
+num_training_episodes = 100  # Define the number of training episodes
