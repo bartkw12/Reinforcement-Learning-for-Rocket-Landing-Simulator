e3fd226d063611f4f0ce7f0e00b83371c58ec151
Bart Kowal <bartkw12@gmail.com>
2025-05-19 12:51:03 -0400

    Starting DQN NN class to create a FF NN in PyTorch - Q-function approximator.

--- src/DQN_model/DQN_model.py
+++ src/DQN_model/DQN_model.py
@@ -9,3 +9,11 @@ import matplotlib.pyplot as plt
 from collections import deque
 from DQN_config import hidden_size, buffer_size, batch_size, gamma, alpha, epsilon, epsilon_decay, num_training_episodes
 
+class DQN(torch.nn.Module):
+    '''
+    Feedforward neural network in PyTorch that can be used as a Q-function approximator for Deep Q-learning.
+    '''
+    def __init__(self, state_size=8, action_size=4, hidden_size=hidden_size):
+        '''
+        Define architecture of NN (two hidden layers).
+        '''
