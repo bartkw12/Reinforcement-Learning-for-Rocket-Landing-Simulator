a3806768919fdc859858b85128fa18539c5232bd
Bart Kowal <bartkw12@gmail.com>
2025-06-17 16:10:48 -0400

    Fixing the early stopping env logic.

--- src/DQN_model/DQN_model.py
+++ src/DQN_model/DQN_model.py
@@ -269,7 +269,7 @@ class DQNAgent:
                     print(f"New best model saved with avg reward: {self.best_avg_reward:.2f}")
 
             # stop environment when reward for LunarLander ~200 (considered solved)
-            if episode < 100 and avg_score < 200:
+            if episode >= 100 and avg_score >= 200:
                 print(f"\nEnvironment solved in {episode} episodes! Average Score: {avg_score:.2f}")
                 self.save_agent(self.best_model_path)
                 print(f"Final model saved: {self.best_avg_reward:.2f}")
