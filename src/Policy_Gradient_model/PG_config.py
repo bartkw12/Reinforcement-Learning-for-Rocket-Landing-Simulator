067df79303a8a30d09b7d305f9533d8f0168a04f
bartkowal <kowalb1@mcmaster.ca>
2025-08-15 12:37:59 -0400

    config changes.

--- src/Policy_Gradient_model/PG_config.py
+++ src/Policy_Gradient_model/PG_config.py
@@ -2,7 +2,7 @@
 
 # REINFORCE Policy Gradient Method
 alpha = 1e-3                  # learning rate - try 0.01 for more gradual learning
-gamma = 0.99                 # discount factor - for more short term focused tasks try 0.8
+gamma = 0.99                  # discount factor - for more short term focused tasks try 0.8
 num_training_episodes = 1000  # Define the number of training episodes
 
 # Hyperparameters specific to REINFORCE
